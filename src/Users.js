b63be38259fc19ab96e23b91c228e6de9ecc47fe
Shivansh Budakoti <shivanshbudakoti03@gmail.com>
2022-04-15 21:00:04 +0530

    craeted satic ui

--- src/Users.js
+++ src/Users.js
@@ -1,13 +1,11 @@
 import React from "react";
-const User = (props) => {
-  const { name, price,index } = props;
-  return (
-    <div className="User" style={{transform:`rotate(calc(40deg * ${index}))`}}>
-      <div style={{left:`calc(0px*${index/2})`,transform:`rotate(calc(-40deg * ${index}))`}}>
-        <span>{name}</span>
-        <span>{price}</span>
-      </div>
-    </div>
-  );
-};
+const User=(props)=>{
+    const {name,price}=props;
+    return(
+        <div className="User">
+         <span>{name}</span>
+         <span>{price}</span>
+        </div>
+    )
+}
 export default User;
